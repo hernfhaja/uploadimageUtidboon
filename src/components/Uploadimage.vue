840b0f0febad4dad870ee27623ca76e50c253bd9
woramun wattana <hernfhaja@gmail.com>
2022-06-04 01:49:20 +0700

    add tailwind

---
+++ src/components/Uploadimage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="UploadImage">
+    <h1 class="text-xm">Upload Images</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UploadImage",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-Vue.config.productionTip = false
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDXSRRTiIEF_zfqjc-wrD_Hg6pLKo6DxCU",
+  authDomain: "uploadimage-11541.firebaseapp.com",
+  projectId: "uploadimage-11541",
+  storageBucket: "uploadimage-11541.appspot.com",
+  messagingSenderId: "939362557510",
+  appId: "1:939362557510:web:312f367ffd871f4c933447",
+  measurementId: "G-HXSC5FJ62K",
+};
+
+// Initialize Firebase
+/* eslint-disable */
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const storage = getStorage(app);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
